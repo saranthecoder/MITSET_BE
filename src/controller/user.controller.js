02084b077f59cdc67b810817676ca0fb8ccca214
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-04-15 21:39:35 +0530

    submit ready

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -59,16 +59,16 @@ export const logInUser = async (req, res) => {
         const isDOBCorrect = user.dateOfBirth.toISOString().split('T')[0] === dateOfBirth;
 
         if (!isDOBCorrect) {
-            return res.status(400).json({success:false, error: "Invalid Date of Birth" });
+            return res.status(400).json({ success: false, error: "Invalid Date of Birth" });
         }
 
         generateTokenSetCookie(user._id, res);
-      
+
         console.log("User Logged In");
-        res.status(200).json({id:user._id,success:true});
+        res.status(200).json({ id: user._id, success: true });
     } catch (error) {
         console.log("Error in Login controller", error.message);
-        res.status(500).json({success:false, error: "Internal Server error" });
+        res.status(500).json({ success: false, error: "Internal Server error" });
     }
 };
 
@@ -95,7 +95,7 @@ export const getUserQP = async (req, res) => {
                 return { questionId, question, options: shuffledOptions };
             }));
 
-            return { subjectId,subject, data: shuffledQuestions };
+            return { subjectId, subject, data: shuffledQuestions };
         });
 
         res.status(200).json({ questions: shuffledResponse });
@@ -109,18 +109,19 @@ export const getUserQP = async (req, res) => {
 export const storeUserAnswerData = async (req, res) => {
 
     let physicsData, chemistryData, mathematicsData;
-    let physicsDataDB, chemistryDataDB, mathematicsDataDB;
-    
+    // let physicsDataDB, chemistryDataDB, mathematicsDataDB;
+
     try {
-        
+
         const hallTicketNo = req.user.hallTicketNo;
         const dateOfBirth = req.user.dateOfBirth
 
         // Extract data from the request body
-        const {questions } = req.body;
+        const { questions } = req.body;
         questions.forEach(element => {
             if (element.subject == 'physics') {
                 physicsData = element.data;
+                console.log(typeof(physicsData));
             } else if (element.subject == 'chemistry') {
                 chemistryData = element.data;
             } else if (element.subject == 'mathematics') {
@@ -128,44 +129,48 @@ export const storeUserAnswerData = async (req, res) => {
             }
         });
 
-        const questionsDB = await Question.find({});
-        questionsDB.forEach(element => {
-            if (element.subject == 'physics') {
-                physicsDataDB = element.data;
-            } else if (element.subject == 'chemistry') {
-                chemistryDataDB = element.data;
-            } else if (element.subject == 'mathematics') {
-                mathematicsDataDB = element.data;
-            }
-        });
 
-        const physicsAnswers = await saveAnswers(physicsData, physicsDataDB);
-        const chemistryAnswers = await saveAnswers(chemistryData, chemistryDataDB);
-        const mathematicsAnswers = await saveAnswers(mathematicsData, mathematicsDataDB);
+        // const questionsDB = await Question.find({});
+        // questionsDB.forEach(element => {
+        //     if (element.subject == 'physics') {
+        //         physicsDataDB = element.data;
+        //         console.log(physicsDataDB);
+        //     } else if (element.subject == 'chemistry') {
+        //         chemistryDataDB = element.data;
+        //     } else if (element.subject == 'mathematics') {
+        //         mathematicsDataDB = element.data;
+        //     }
+        // });
+
+        // const physicsAnswers = await saveAnswers(physicsData, physicsDataDB);
+        // const chemistryAnswers = await saveAnswers(chemistryData, chemistryDataDB);
+        // const mathematicsAnswers = await saveAnswers(mathematicsData, mathematicsDataDB);
 
         const newUserAnswer = new UserAnswer({
             hallTicketNo,
             dateOfBirth: new Date(dateOfBirth),
             questions: [
-                { subject: 'physics', data: physicsAnswers },
-                { subject: 'chemistry', data: chemistryAnswers },
-                { subject: 'mathematics', data: mathematicsAnswers },
+                { subject: 'physics', data: physicsData},
+                { subject: 'chemistry', data: chemistryData },
+                { subject: 'mathematics', data: mathematicsData },
             ],
         });
 
         // Save the new UserAnswer to the database  
         await newUserAnswer.save();
 
-        res.cookie("jwt","",{maxAge:0}); // token got expire
+        res.cookie("jwt", "", { maxAge: 0 }); // token got expire
 
         res.status(201).json({ success: true, message: 'Hall ticket data saved successfully.' });
     } catch (error) {
         console.error('Error saving hall ticket data:', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-    
+
 };
 
+
+/*
 async function saveAnswers(data, dataDB) {
     try {
         const answers = [];
@@ -193,3 +198,5 @@ async function saveAnswers(data, dataDB) {
         console.error("Error in saveAnswers function:", error);
     }
 }
+
+*/
--- src/models/userAnswer.model.js
+++ src/models/userAnswer.model.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
 // Define schema for questions
-const QuestionSchema = new mongoose.Schema({
-    question: String,
-    options: String
-});
+// const QuestionSchema = new mongoose.Schema({
+//     question: String,
+//     options: String
+// });
 
 // Define schema for subjects
 const SubjectSchema = new mongoose.Schema({
     subject: String,
-    data: [QuestionSchema]
+    data: Object
 });
 
 // Define schema for storing hall ticket information
